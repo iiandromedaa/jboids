3be344f843842d525f9a546395825294947c8fc1
andro <90076821+iiandromedaa@users.noreply.github.com>
2026-08-27 16:50:54 -0500

    init

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import javax.swing.SwingUtilities;
+
+import gui.JBoidsGUI;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new JBoidsGUI();
+            }
+        });
+    }
+
+}
---
+++ src/gui/JBoidsGUI.java
@@ -0,0 +1,16 @@
+package gui;
+
+public class JBoidsGUI {
+    
+    private static final int DEFAULT_WIDTH = 500;
+    private static final int DEFAULT_HEIGHT = 500;
+
+    public JBoidsGUI() {
+        this(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+    }
+
+    public JBoidsGUI(int width, int height) {
+        
+    }
+    
+}
